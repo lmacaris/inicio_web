13643dd9eaa7c105df2623394f6cf06465bab603
lmacaris <hihi.cod@gmail.com>
2024-04-27 16:02:32 +0800

    loader

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// loader - usamos un setTimeout para que dure 2 secs y despues desaparezca cambiando el display de navbar y footer a "flex"
+setTimeout(() => {
+  document.querySelector(".loader").classList.add("loader--hidden");
+  document.querySelector(".navbar").style.display = "flex";
+  document.querySelector(".footer").style.display = "flex";
+}, 2000);
+// -----------------------------------------------
+// --------------------- MENU y Sandwich menu --------------------------
+
 const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown_menu");
@@ -8,3 +17,5 @@ toggleBtn.onclick = () => {
 
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
+
+//
